e6ed5537b540b51f587a18bbb4f6a0129c0a667a
nasirdaudahmad <nasirdaudahmad@saapk.org>
2025-04-30 14:08:44 +0500

    may be some new changes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -14,7 +14,7 @@ const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustom
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        Dashboard (Overview)
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
